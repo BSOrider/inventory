83fe53ed73424b066680e845d5e9295b59f655c4
Ike <carmanwatts@gmail.com>
2017-05-03 21:13:52 -0300

    setup for total management

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -8,17 +8,13 @@ $('#upload-input').on('change', function() {
 
   if (files.length > 0) {
     // create a FormData object which will be sent as the data payload in the
-    // AJAX request
     var formData = new FormData();
-
     // loop through all the selected files and add them to the formData object
     for (var i = 0; i < files.length; i++) {
       var file = files[i];
-
       // add the files to formData object for the data payload
       formData.append('uploads[]', file, file.name);
     }
-
     var password = $("#password").val();
     var price = $("#price").val();
     var width = $("#width").val();
