c4a3cc283cdd46b9520bac562be46f59c661f432
Ike <carmanwatts@gmail.com>
2017-05-05 21:06:49 -0300

    consolidated ajax stuff

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,3 +1,19 @@
+const request = function(url, data){
+  $.ajax({
+    url: url,
+    type: 'POST',
+    data: data,
+    processData: false,
+    contentType: '',
+    success: function(data) {
+      window.location.reload();
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      alert('authentication failed!');
+    }
+  });
+};
+
 $('.upload-btn').on('click', function() {
   $('#upload-input').click();
 });
@@ -21,19 +37,10 @@ $('#upload-input').on('change', function() {
     var padding = $("#padding").val();
     var name = $("#name").val();
 
-    $.ajax({
-      url: '/saveSaddle?token=' + password + '&price=' + price + '&width=' + width + '&padding=' + padding + '&name=' + name,
-      type: 'POST',
-      data: formData,
-      processData: false,
-      contentType: '',
-      success: function(data) {
-        window.location.reload();
-      },
-      error: function(XMLHttpRequest, textStatus, errorThrown) {
-        alert('authentication failed!');
-      }
-    });
+    var url = '/saveSaddle?token=' + password + '&price=' + price + '&width=' + width + '&padding=' + padding + '&name=' + name;
+
+    request(url, formData);
+
   }
 });
 
@@ -44,17 +51,10 @@ $('.delete').on('click', function() {
   var image = $(this).prev().text();
   var saddleId = $(this).prev().prev().text();
 
-  $.ajax({
-    url: '/deleteSaddle?token=' + password + '&id=' + saddleId + '&image=' + image,
-    type: 'POST',
-    data: {},
-    processData: false,
-    contentType: '',
-    success: function(data) {
-      window.location.reload();
-    },
-    error: function(XMLHttpRequest, textStatus, errorThrown) {
-      alert('authentication failed!');
-    }
-  });
+  var url = '/deleteSaddle?token=' + password + '&id=' + saddleId + '&image=' + image;
+  var data = {};
+
+  request(url, data);
+
 });
+
