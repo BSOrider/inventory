3cd86d8f6e5cb841c26367ccc90c866f70beb4bc
BSOrider <carmanwatts@gmail.com>
2017-12-02 18:01:17 -0300

    fixes, improvements

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var formidable = require('formidable');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var fs = require('fs');
-var mongodb = require("mongodb");
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var app = express();
-var connection = require('./lib/db.js');
+const express = require('express');
+const formidable = require('formidable');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const fs = require('fs');
+const mongodb = require("mongodb");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const app = express();
+const connection = require('./lib/db.js');
+const multer = require('multer');
+const upload = multer({ dest: 'public/saddlePics' });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +22,9 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 /* =================================================== */
@@ -30,7 +34,7 @@ app.use(bodyParser.json());
 /* =================================================== */
 
 /* home page */
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   console.log(new Date());
   res.render('index', {
     "pageType": "homepage",
@@ -39,7 +43,7 @@ app.get('/', function(req, res) {
 });
 
 /* about page */
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
   console.log(new Date());
   res.render('about', {
     "pageType": "no-sidebar",
@@ -48,7 +52,7 @@ app.get('/about', function(req, res) {
 });
 
 /* analysis page */
-app.get('/analysis', function(req, res) {
+app.get('/analysis', function (req, res) {
   console.log(new Date());
   res.render('analysis', {
     "pageType": "no-sidebar",
@@ -57,11 +61,11 @@ app.get('/analysis', function(req, res) {
 });
 
 /* saddles page */
-app.get('/saddles', function(req, res) {
+app.get('/saddles', function (req, res) {
   console.log(new Date());
-  connection(function(db) {
+  connection(function (db) {
     var collection = db.collection('saddles');
-    collection.find({}).toArray(function(error, saddles) {
+    collection.find({}).toArray(function (error, saddles) {
       if (error) {
         console.log(error);
       } else {
@@ -82,11 +86,11 @@ app.get('/saddles', function(req, res) {
 /* =================================================== */
 
 /* admin page */
-app.get('/admin', function(req, res) {
+app.get('/admin', function (req, res) {
   console.log(new Date());
-  connection(function(db) {
+  connection(function (db) {
     var collection = db.collection('saddles');
-    collection.find({}).toArray(function(error, saddles) {
+    collection.find({}).toArray(function (error, saddles) {
       if (error) {
         console.log(error);
       } else {
@@ -97,24 +101,25 @@ app.get('/admin', function(req, res) {
 });
 
 /* delete saddle */
-app.post('/deleteSaddle', function(req, res) {
+app.post('/deleteSaddle', function (req, res) {
   console.log(new Date());
   var itemToDelete = req.query.id;
   var token = req.query.token;
   var image = req.query.image;
-  connection(function(db) {
+  connection(function (db) {
     var collection = db.collection('validation');
-    collection.find({}).toArray(function(error, docs) {
+    collection.find({}).toArray(function (error, docs) {
       var password = docs[0].password;
       if (error) {
         console.log(error);
       } else if (token == password) {
-        connection(function(db) {
+        connection(function (db) {
           var collection = db.collection('saddles');
           collection.deleteOne({ _id: new mongodb.ObjectID(itemToDelete) });
-          fs.unlink('./public/saddlePics/' + image, function(err) {
+          fs.unlink('./public/saddlePics/' + image, function (err) {
             if (err) {
               console.log(err);
+              res.end('success');
             } else {
               res.end('success');
             }
@@ -130,65 +135,52 @@ app.post('/deleteSaddle', function(req, res) {
 });
 
 /* save saddle image */
-app.post('/saveSaddle', function(req, res) {
+app.post('/saveSaddle', upload.single('file'), function (req, res) {
   console.log(new Date());
-  var token = req.query.token;
+  var token = req.body.password;
   var newSaddle = {
-    name: req.query.name,
-    price: req.query.price,
-    width: req.query.width,
-    padding: req.query.padding,
-    condition: req.query.condition
+    name: req.body.name,
+    price: req.body.price,
+    width: req.body.width,
+    padding: req.body.padding,
+    condition: req.body.condition
   };
-  connection(function(db) {
+  connection(function (db) {
     var collection = db.collection('validation');
-    collection.find({}).toArray(function(error, docs) {
+    collection.find({}).toArray(function (error, docs) {
       var password = docs[0].password;
-      if (error) {
-        console.log(error);
-      } else if (token == password) {
+      if (token == password) {
         console.log("validated!");
-        // prep image name holder
-        var imgName = null;
-        var form = new formidable.IncomingForm();
-        form.multiples = true;
-        form.uploadDir = path.join(__dirname, './public/saddlePics');
-        form.on('file', function(field, file) {
-          // get image name to be saved along with rest of data
-          imgName = file.name;
-          // rename to correct name
-          fs.rename(file.path, path.join(form.uploadDir, file.name));
-        });
-        form.on('error', function(err) {
-          console.log('An error has occured: \n' + err);
-        });
-        form.on('end', function() {
+        let imageName = req.file.originalname;
+        console.log(imageName);
+        // rename to correct name
+        fs.rename(req.file.path, path.join(req.file.destination, req.file.originalname), function (err, result) {
+          console.log(result);
           // add pic name to saddle data
-          newSaddle.image = imgName;
+          newSaddle.image = imageName;
           // save data
           var collection = db.collection('saddles');
           // save saddle data in collection
-          collection.insert([newSaddle], function(err, result) {
+          collection.insert([newSaddle], function (err, result) {
             if (err) {
               console.log(err);
             } else {
               console.log("new saddle saved!");
-              res.end('success');
+              return res.redirect('admin');
             }
           });
         });
-        form.parse(req);
       } else {
         console.log("rejected!");
         // once recieved causes redirecion to 'stuffed' page
-        res.sendStatus(500);
+        // return res.redirect(500);
       }
     });
   });
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   console.log("should be 404");
   console.log(new Date());
   var err = new Error('Not Found');
@@ -197,7 +189,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,4 +1,5 @@
 const request = function(url, data) {
+  console.log(data);
   $.ajax({
     url: url,
     type: 'POST',
@@ -14,36 +15,38 @@ const request = function(url, data) {
   });
 };
 
-$('.upload-btn').on('click', function() {
-  $('#upload-input').click();
-});
+// $('.upload-btn').on('click', function() {
+//   $('#upload-input').click();
+// });
 
-$('#upload-input').on('change', function() {
+// $('#upload-input').on('change', function() {
 
-  var files = $(this).get(0).files;
+//   var files = $(this).get(0).files;
 
-  if (files.length > 0) {
-    // create a FormData object which will be sent as the data payload in the
-    var formData = new FormData();
-    // loop through all the selected files and add them to the formData object
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
-      // add the files to formData object for the data payload
-      formData.append('uploads[]', file, file.name);
-    }
-    var password = $("#password").val();
-    var price = $("#price").val();
-    var width = $("#width").val();
-    var padding = $("#padding").val();
-    var name = $("#name").val();
+//   if (files.length > 0) {
+//     // create a FormData object which will be sent as the data payload in the
+//     var formData = new FormData();
+//     // loop through all the selected files and add them to the formData object
+//     for (var i = 0; i < files.length; i++) {
+//       var file = files[i];
+//       // add the files to formData object for the data payload
+//       formData.append('uploads[]', file, file.name);
+//     }
+//     formData.token = $("#password").val();
+//     formData.price = $("#price").val();
+//     formData.width = $("#width").val();
+//     formData.padding = $("#padding").val();
+//     formData.name = $("#name").val();
 
-    var url = '/saveSaddle?token=' + password + '&price=' + price + '&width=' + width + '&padding=' + padding + '&name=' + name;
+//     // var url = '/saveSaddle?token=' + password + '&price=' + price + '&width=' + width + '&padding=' + padding + '&name=' + name;
+//     var url = '/saveSaddle';
 
-    request(url, formData);
+//     console.log(formData);
 
-  }
-});
+//     return request(url, formData);
 
+//   }
+// });
 
 $('.delete').on('click', function() {
   var password = $("#password").val();
@@ -51,5 +54,5 @@ $('.delete').on('click', function() {
   var saddleId = $(this).prev().prev().text();
   var url = '/deleteSaddle?token=' + password + '&id=' + saddleId + '&image=' + image;
   var data = {};
-  request(url, data);
+  return request(url, data);
 });
